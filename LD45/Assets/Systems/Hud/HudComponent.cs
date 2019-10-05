4d900ba8ba34ac18f521efb89432a60a7638eb8a
Daniel Varela <df.varela@outlook.com>
2019-10-05 17:55:21 +0200

    Addedhud system and component

---
+++ LD45/Assets/Systems/Hud/HudComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SystemBase;
+
+namespace Assets.Systems.Hud
+{
+    public class HudComponent : GameComponent
+    {
+        public List<HudKey> AvailableKeys;
+    }
+}
---
+++ LD45/Assets/Systems/Hud/HudKey.cs
@@ -0,0 +1,12 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Assets.Systems.Hud
+{
+    public class HudKey
+    {
+        public string KeyValue;
+        public Sprite KeyThumbnail;
+        public BoolReactiveProperty IsActive = new BoolReactiveProperty(false);
+    }
+}
