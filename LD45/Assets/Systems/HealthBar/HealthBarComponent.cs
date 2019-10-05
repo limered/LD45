a4a6fd8444ae8d87b0c01dab6c4baceb1eddd708
Helen <helen-bohleber@web.de>
2019-10-05 20:34:42 +0200

    Show heart in test scene

---
+++ LD45/Assets/Systems/HealthBar/HealthBarComponent.cs
@@ -0,0 +1,31 @@
+﻿using SystemBase;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Systems.HealthBar
+{
+    public class HealthBarComponent : GameComponent
+    {
+        public int MaxHealthShown = 3;
+        public int CurrentHealthShown = 3;
+        public Image[] HeartImages;
+        public Sprite FullHeart;
+        public Sprite EmptyHeart;
+
+        public void SetSpriteOfHeart()
+        {
+            for(int i = 0; i < MaxHealthShown; i++)
+            {
+                if(i < CurrentHealthShown)
+                {
+                    HeartImages[i].sprite = FullHeart;
+                }
+                else
+                {
+                    HeartImages[i].sprite = EmptyHeart;
+                }
+            }
+
+        }
+    }
+}
