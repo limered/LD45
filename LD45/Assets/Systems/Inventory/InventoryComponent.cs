a061062ebaa91f32d549c6ba82ca926c73f8aea9
Helen <helen-bohleber@web.de>
2019-10-07 13:00:16 +0200

    Add room with nothing keys

--- LD45/Assets/Systems/Inventory/InventoryComponent.cs
+++ LD45/Assets/Systems/Inventory/InventoryComponent.cs
@@ -11,13 +11,7 @@ namespace Systems.Inventory
         public ReactiveProperty<List<char>> CollectedKeys = new ReactiveProperty<List<char>>(
             new List<char>
         {
-            'n',
-            'o',
-            't',
-            'h',
-            'i',
-            'n',
-            'g'
+            
         });
     }
 }
