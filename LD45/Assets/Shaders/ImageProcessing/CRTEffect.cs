c57833783796b0b3f32096820e03d52aa20ab31f
Emil Wasilewski <me@limered.de>
2019-10-07 15:22:51 +0200

    shader base

---
+++ LD45/Assets/Shaders/ImageProcessing/CRTEffect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Shaders.ImageProcessing
+{
+    public class CRTEffect:MonoBehaviour
+    {
+        public Material material;
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(source, destination, material);
+        }
+    }
+}
